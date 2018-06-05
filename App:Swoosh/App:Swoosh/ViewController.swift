27a7f04c7fd319ce1a0237ccf45df69de12e937a
parthlmb1 <parthlmb007@gmail.com>
2018-06-05 13:51:04 +0530

    Layouts fixed

--- App:Swoosh/App:Swoosh/ViewController.swift
+++ App:Swoosh/App:Swoosh/ViewController.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swooshLogo: UIImageView!
+    
+    @IBOutlet weak var swooshBg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        //Pixle perfect constraints
+        
+        /*swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 20, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.width)
+    
+        swooshBg.frame = view.frame*/
+        
     }
-
+    
     // Make the Status Bar Light/Dark Content for this View
     override var preferredStatusBarStyle : UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
         //return UIStatusBarStyle.default   // Make dark again
     }
 }
-
