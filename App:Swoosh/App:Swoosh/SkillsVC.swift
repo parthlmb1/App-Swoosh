bd7ef0f72ec408bc83303b46834dcd053de595e5
parthlmb1 <parthlmb007@gmail.com>
2018-06-13 00:50:37 +0530

    refactoring done

---
+++ App:Swoosh/App:Swoosh/SkillsVC.swift
@@ -0,0 +1,35 @@
+//
+//  SkillsVC.swift
+//  App:Swoosh
+//
+//  Created by Parth Lamba on 13/06/18.
+//  Copyright © 2018 Parth Lamba. All rights reserved.
+//
+
+import UIKit
+
+class SkillsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
